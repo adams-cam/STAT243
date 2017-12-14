49171e0fb84e5dbcfdc0a98c2c899a38c81a61f2
808cam <cam.adams@berkeley.edu>
2017-12-13 23:25:10 -0800

    Add existing file

--- project/GA/tests/testthat/test-select.R
+++ project/GA/tests/testthat/test-select.R
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 context('selet')
 
 y <- mtcars$mpg
@@ -47,25 +46,25 @@ x <- as.matrix(mtcars[,c(-1)])
 test_that('Test the invalid inputs',{
   expect_error(select(y, x, family = "gaussian", nCores = 1.5))
   # invalid input for 'nCores', which should be integer
-  
+
   expect_error(select(y, x, family = "gaussian", pCrossover = 1.2))
   # invalid input for 'pCrossover', which should be integer
-  
+
   expect_error(select(y, x, family = "gaussian", pCrossover = 0.2))
   # invalid input for 'pCrossover', which should be integer
-  
+
   expect_error(select(y, x, family = "gaussian", start_chrom = 10.5))
   # invalid input for 'start_chrom', which should be integer
-  
+
   expect_error(select(y, x, family = "gaussian", mutation_rate = 2))
   # invalid input for 'mutation_rate', which should be integer
-  
+
   expect_error(select(y, x, family = "gaussian", mutation_rate = 0.5))
   # invalid input for 'mutation_rate', which should be integer
-  
+
   expect_error(select(y, x, family = "gaussian", iter = 25.5))
   # invalid input for 'iter', which should be integer
-  
+
   expect_error(select(x, family = "gaussian"))
   # missing input for 'y', which should be a vector
 })
@@ -73,10 +72,10 @@ test_that('Test the invalid inputs',{
 test_that('Test different family',{
   y1 <- sample(c(0,1),32,replace = TRUE)
   # create a new response variable for binomial and poisson family
-  
+
   expect_output(select(y1, x, family = "binomial"))
   # test the whether the binomial family works for the function
-  
+
   expect_output(select(y1, x, family = "poisson"))
   # test the whether the poisson family works for the function
 })
